f1b1534480014ddaef39b73243fa4d234bd0b2f1
YongHyun Lee <mysticanimal@gmail.com>
2014-04-29 13:25:05 +0900

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name     "YongHyun Lee"
-		email    "test@example.com"
+		sequence(:name)		{ |n| "Pserson_#{n}" }
+		sequence(:email)	{ |n| "Pserson_#{n}@example.com" }
 		password "foobar"
 		password_confirmation "foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
